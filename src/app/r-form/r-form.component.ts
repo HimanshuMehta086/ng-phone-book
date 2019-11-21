0ea54c9e35640adb446e833e657fc4ca0015038d
Vivekanand P V <vivekanandpv@outlook.com>
2019-11-21 22:35:50 +0530

    done

--- src/app/r-form/r-form.component.ts
+++ src/app/r-form/r-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-r-form',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./r-form.component.css']
 })
 export class RFormComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      telephone: new FormControl('', Validators.maxLength(11)),
+      mobilephone: new FormControl('', Validators.maxLength(11)),
+      email: new FormControl('', Validators.email),
+      category: new FormControl('', Validators.required)
+    });
   }
 
+  get name() {
+    return this.form.controls['name'];
+  }
+
+  get telephone() {
+    return this.form.controls['telephone'];
+  }
+
+  get mobilephone() {
+    return this.form.controls['mobilephone'];
+  }
+
+  get email() {
+    return this.form.controls['email'];
+  }
+
+  get category() {
+    return this.form.controls['category'];
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
